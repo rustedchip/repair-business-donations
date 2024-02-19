685d215739536be0a3f6c2ee1491294d6b530707
rustedchip <rustedchip@gmail.com>
2024-02-18 23:09:07 -0800

    package :: service-provider

---
+++ src/RepairBusinessDonationsServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Rustedchip\RepairBusinessDonations;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepairBusinessDonationsServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+
+ 
+    }
+
+    public function boot()
+    {
+ 
+        
+    }
+    
+    protected function registerConfig()
+    {
+ 
+    }
+}
