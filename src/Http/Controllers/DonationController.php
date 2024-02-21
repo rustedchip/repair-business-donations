6c2d908b820f9dbfbaee4ce2d37b6111a9b26841
rustedchip <rustedchip@gmail.com>
2024-02-21 00:35:22 -0800

    app :: controllers

--- src/Http/Controllers/DonationController.php
+++ src/Http/Controllers/DonationController.php
@@ -3,15 +3,11 @@
 namespace Rustedchip\RepairBusinessDonations\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Auth;
 
 class DonationController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    
+   
+
     public function index_donation()
     {
         return view('repair-business-donations::index-donation');
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use Rustedchip\RepairBusinessDonations\Http\Controllers\DonationController;
 
-Route::get('/donations', [DonationController::class, 'index_donation'])->name('index_donation');
+Route::get('/donations', [DonationController::class, 'index_donation'])->name('index_donation')->middleware('admin')->middleware('verified')->middleware('notification'); 
