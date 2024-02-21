85bab31699f703b10eb8e80578d7a762620ba035
rustedchip <rustedchip@gmail.com>
2024-02-21 00:40:49 -0800

    app :: controllers

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Rustedchip\RepairBusinessDonations\Http\Controllers\DonationController;
 
-Route::get('/donations', [DonationController::class, 'index_donation'])->name('index_donation')->middleware('admin')->middleware('verified')->middleware('notification'); 
+Route::get('/donations', [DonationController::class, 'index_donation'])->name('index_donation')->middleware('admin')->middleware('verified')->middleware('notification')->middleware('web');
