b4f71d3f13fe4a39678b44eee5566a10bc685426
rustedchip <rustedchip@gmail.com>
2024-02-21 00:29:58 -0800

    app :: controllers

--- src/RepairBusinessDonationsServiceProvider.php
+++ src/RepairBusinessDonationsServiceProvider.php
@@ -20,7 +20,7 @@ class RepairBusinessDonationsServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->loadViewsFrom(__DIR__.'resources/views', 'repair-business-donations');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'repair-business-donations');
  
         
     }
